54dcd815d6075eda9b64776b035d8e794ee3cfb1
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-03-29 20:41:41 -0400

    coomit-2

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
+// Declare variables for DOM elements
 let inputForm = document.querySelector('#inputForm');
 let textInput = document.querySelector('#textInput');
-let todolist = document.querySelector('.todolist');
+let button = document.querySelector('#deleteAll');
+let todolist = document.querySelector('#todolist');
 
-inputForm.addEventListener('submit', function(event){
-    event.preventDefault()
+//EVENT LISTENERS
+inputForm.addEventListener('submit', submitHandler)
+
+button.addEventListener('click', deleteAllHandler)
+
+todolist.addEventListener('click', strikethrough)
+
+//Function to append text input to the ordered list as a new li
+function submitHandler(event){
+    event.preventDefault();
     let newLi = document.createElement("li");
     newLi.innerText = textInput.value;
     todolist.appendChild(newLi);
     textInput.value = '';
-})
+}
+
+//Function to replace all li's with blank if li's exist within parent list
+function deleteAllHandler(event){
+    //document.getElementById('todolist').innerHTML = '';
+    if (todolist.hasChildNodes){
+    todolist.replaceChildren();
+    }
+}
+
+//Function to srtikethrough li's 
+function strikethrough(event){
+    if (event.target.style.textDecoration === 'line-through'){
+        event.target.style.textDecoration = '';
+    } else {
+        event.target.style.textDecoration = 'line-through';
+    } 
+}
+
+
+
+
+
+
